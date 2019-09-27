393d2b4c8b8e5a982f6d3bc35ca474fac8ce82c1
JakubRZE <jakub.rzm@gmail.com>
2019-09-27 20:49:21 +0200

    add dbcontext

---
+++ backend/about_me_Web_API/about_me_Web_API/Models/EventDetail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace about_me_Web_API.Models
+{
+    public class EventDetail
+    {
+    }
+}
--- backend/about_me_Web_API/about_me_Web_API/Startup.cs
+++ backend/about_me_Web_API/about_me_Web_API/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using about_me_Web_API.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,6 +26,9 @@ namespace about_me_Web_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
