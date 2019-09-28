9ad75627dc1005d2c0c2e307ba70420f88400e6d
JakubRZE <jakub.rzm@gmail.com>
2019-09-28 21:32:13 +0200

    add timeline component

--- frontend/about-me-Angular8/src/app/app.module.ts
+++ frontend/about-me-Angular8/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { TimelineEventComponent } from './timeline/timeline-event/timeline-event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent,
+    TimelineEventComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/about-me-Angular8/src/app/timeline/timeline-event/timeline-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline-event',
+  templateUrl: './timeline-event.component.html',
+  styleUrls: ['./timeline-event.component.css']
+})
+export class TimelineEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
