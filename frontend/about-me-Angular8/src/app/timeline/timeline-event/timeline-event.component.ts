9dc297cac5d4035b7eaef68c1c4ffd3b6b56f5c4
Jakub Rzepka <Jakubjakk@gmail.com>
2019-10-13 20:29:48 +0200

    timeline update

--- frontend/about-me-Angular8/src/app/timeline/timeline-event/timeline-event.component.ts
+++ frontend/about-me-Angular8/src/app/timeline/timeline-event/timeline-event.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineEventComponent implements OnInit {
 
+  timelineEvents: { id: number, title: string, description: string, img: string }[] = [
+    {
+      "id": 1,
+      "title": "WorkSample event.",
+      "description": "Once upon a time.. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "img": "https://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_t1/sample_images/img/index/ff_x_t1_001.JPG"
+    },
+    {
+      "id": 1,
+      "title": "WorkSample event.",
+      "description": "Once upon a time.. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "img": "https://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_t1/sample_images/img/index/ff_x_t1_001.JPG"
+    },
+    {
+      "id": 1,
+      "title": "WorkSample event.",
+      "description": "Once upon a time.. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "img": "https://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_t1/sample_images/img/index/ff_x_t1_001.JPG"
+    },
+    {
+      "id": 1,
+      "title": "WorkSample event.",
+      "description": "Once upon a time.. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "img": "https://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_t1/sample_images/img/index/ff_x_t1_001.JPG"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
--- frontend/about-me-Angular8/src/app/timeline/timeline.component.ts
+++ frontend/about-me-Angular8/src/app/timeline/timeline.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineComponent implements OnInit {
 
+  timelineHeaders: { id: number, title: string, description: string, img: string }[] = [
+    {
+      "id": 1,
+      "title": "Work",
+      "description": "A little bit about work I experienced so far.",
+      "img": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201810/online-3412473_1920.jpeg?meGHIUut1mybIL3pem8eWqk34osmW3Zi"
+    },
+    {
+      "id": 2,
+      "title": "Education",
+      "description": "Something about my education path.",
+      "img": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201810/online-3412473_1920.jpeg?meGHIUut1mybIL3pem8eWqk34osmW3Zi"
+    },
+    {
+      "id": 3,
+      "title": "Programming",
+      "description": "All my project I menaged to create so far.",
+      "img": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201810/online-3412473_1920.jpeg?meGHIUut1mybIL3pem8eWqk34osmW3Zi"
+    },
+    {
+      "id": 4,
+      "title": "After Hours",
+      "description": "My hobbies and life after hours.",
+      "img": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201810/online-3412473_1920.jpeg?meGHIUut1mybIL3pem8eWqk34osmW3Zi"
+    }
+  ];
+
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
